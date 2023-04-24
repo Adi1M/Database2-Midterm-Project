0f9fecdcd87954a2194afa77d49ed875b97e982b
Syrhundred <91780657+Syrhundred@users.noreply.github.com>
2023-04-25 01:52:11 +0600

    changes

--- Queries/functions.sql
+++ Queries/functions.sql
@@ -1,31 +1,114 @@
--- Function which counts the number of records
-CREATE OR REPLACE FUNCTION count_records(table_name VARCHAR)
-    RETURNS INT
-AS
-$$
-DECLARE
-    record_count INT;
-BEGIN
-    EXECUTE 'SELECT count(*) FROM ' || table_name INTO record_count;
-    RETURN record_count;
-END
-$$
-    LANGUAGE plpgsql;
-
-
 -- Add user-defined exception which disallows to enter title of item (e.g. book) to be less than 5 characters
-CREATE OR REPLACE FUNCTION check_title_length(title text)
+CREATE
+    OR REPLACE FUNCTION check_title_length(title text)
     RETURNS void AS
 $$
 BEGIN
-    IF CHAR_LENGTH(title) < 5 THEN
+    IF
+        CHAR_LENGTH(title) < 5 THEN
         RAISE EXCEPTION 'Title must be at least 5 characters long.';
     END IF;
 END;
-$$ LANGUAGE plpgsql;
+$$
+    LANGUAGE plpgsql;
 
 CREATE TRIGGER check_title_length_trigger
     BEFORE INSERT
     ON Book
     FOR EACH ROW
 EXECUTE FUNCTION check_title_length(NEW.title);
+
+-- Function which counts the number of records
+CREATE OR REPLACE FUNCTION show_row_count()
+RETURNS TRIGGER AS $$
+DECLARE
+    row_count INTEGER;
+BEGIN
+    SELECT COUNT(*) INTO row_count FROM AUTHOR;
+    RAISE NOTICE 'Current number of rows in the table: %', row_count;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION show_row_count1()
+RETURNS TRIGGER AS $$
+DECLARE
+    row_count INTEGER;
+BEGIN
+    SELECT COUNT(*) INTO row_count FROM BOOK;
+    RAISE NOTICE 'Current number of rows in the table: %', row_count;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION show_row_count2()
+RETURNS TRIGGER AS $$
+DECLARE
+    row_count INTEGER;
+BEGIN
+    SELECT COUNT(*) INTO row_count FROM category;
+    RAISE NOTICE 'Current number of rows in the table: %', row_count;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION show_row_count3()
+RETURNS TRIGGER AS $$
+DECLARE
+    row_count INTEGER;
+BEGIN
+    SELECT COUNT(*) INTO row_count FROM CUSTOMER;
+    RAISE NOTICE 'Current number of rows in the table: %', row_count;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION show_row_count4()
+RETURNS TRIGGER AS $$
+DECLARE
+    row_count INTEGER;
+BEGIN
+    SELECT COUNT(*) INTO row_count FROM orderdetail;
+    RAISE NOTICE 'Current number of rows in the table: %', row_count;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION show_row_count5()
+RETURNS TRIGGER AS $$
+DECLARE
+    row_count INTEGER;
+BEGIN
+    SELECT COUNT(*) INTO row_count FROM orderheader;
+    RAISE NOTICE 'Current number of rows in the table: %', row_count;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION show_row_count6()
+RETURNS TRIGGER AS $$
+DECLARE
+    row_count INTEGER;
+BEGIN
+    SELECT COUNT(*) INTO row_count FROM publisher;
+    RAISE NOTICE 'Current number of rows in the table: %', row_count;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION change_quantity()
+RETURNS TRIGGER AS $$
+DECLARE
+    detail_quan int;
+    recent_id int;
+BEGIN
+    SELECT NEW.quantity, new.book_id INTO detail_quan, recent_id FROM orderdetail, BOOK
+    WHERE NEW.BOOK_ID = BOOK.book_id;
+    UPDATE book set quantity = quantity - detail_quan where book_id = recent_id;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
--- Queries/trigger.sql
+++ Queries/trigger.sql
@@ -1,1 +1,40 @@
 -- Create a trigger before insert on any entity which will show the current number of rows in the table
+CREATE or REPLACE TRIGGER before_insert_show_row_count
+BEFORE INSERT ON AUTHOR
+FOR EACH ROW
+EXECUTE FUNCTION show_row_count();
+
+CREATE or REPLACE TRIGGER before_insert_show_row_count1
+BEFORE INSERT ON book
+FOR EACH ROW
+EXECUTE FUNCTION show_row_count1();
+
+CREATE OR REPLACE TRIGGER before_insert_show_row_count2
+BEFORE INSERT ON category
+FOR EACH ROW
+EXECUTE FUNCTION show_row_count2();
+
+CREATE OR REPLACE TRIGGER before_insert_show_row_count3
+BEFORE INSERT ON customer
+FOR EACH ROW
+EXECUTE FUNCTION show_row_count3();
+
+CREATE OR REPLACE TRIGGER before_insert_show_row_count4
+BEFORE INSERT ON orderdetail
+FOR EACH ROW
+EXECUTE FUNCTION show_row_count4();
+
+CREATE OR REPLACE TRIGGER before_insert_show_row_count5
+BEFORE INSERT ON orderheader
+FOR EACH ROW
+EXECUTE FUNCTION show_row_count5();
+
+CREATE OR REPLACE TRIGGER before_insert_show_row_count6
+BEFORE INSERT ON publisher
+FOR EACH ROW
+EXECUTE FUNCTION show_row_count6();
+
+CREATE OR REPLACE TRIGGER CHANGE_NUMBEROF_QUANTITY
+AFTER INSERT ON orderdetail
+FOR EACH ROW
+EXECUTE FUNCTION CHANGE_QUANTITY();
